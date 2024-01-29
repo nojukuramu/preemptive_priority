10a84e89545784c4d2c40e44d8eb3882154f5ca4
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-01-30 00:13:21 +0800

    Update pep.py
    Adjusted Some error (still have a situational error)

--- pep.py
+++ pep.py
@@ -8,9 +8,15 @@ job_input = input("Enter first job / type 'confirm' to finish: ")
 
 # Sample data if no inputs are given
 sample_jobs = {
-    'A': [0, 3, 3],
-    'B': [2, 4, 2],
-    'C': [5, 1, 1]
+    'A': [8, 8, 2],
+    'B': [3, 6, 3],
+    'C': [14, 9, 1],
+    'D': [0, 5, 4],
+    'E': [17, 8, 1],
+    'F': [5, 5, 3],
+    'G': [12, 8, 2],
+    'H': [0, 7, 4]
+
 }
 
 # Splits the input
@@ -24,6 +30,9 @@ if not jobs:
     jobs = sample_jobs
 
 # initializations
+for job, values in jobs.items():
+    jobs[job] = [values[0], values[1], values[2], values[1]]
+
 inputtedJobs = f'JOBS: {jobs}'
 copyJobs = copy.deepcopy(jobs)
 remainingJobs = copy.deepcopy(jobs)
@@ -33,6 +42,7 @@ ongoing = {}
 completed = {}
 tat = 0
 wT = 0
+prev_queue = {}
 
 eta = 0
 for item in jobs.values():
@@ -40,22 +50,19 @@ for item in jobs.values():
 
 # Insert your logic here
 while True:
-    
+
     for job, details in jobs.items():
         if details[0] == currTime:
             queue[job] = details
 
-    #Sorting the queue list according to this respectively: Priority>Bursttime>Name
-    queue = dict(sorted(queue.items(), key=lambda x: x[1][2]))
+    
 
-    #if ongoing is empty and queue isn't, transfer first item in queue to ongoing
-    if not ongoing and queue:
-        item = min(queue, key=lambda x: queue[x][2])
-        ongoing = {item: queue[item]}
-        del queue[item]
+    
         
+    
+
     #if queue got new job, ongoing job will be pulled back to queue
-    elif ongoing and queue:
+    if ongoing and queue and (queue != prev_queue):
         for job, details in ongoing.items():
             queue[job] = details
         ongoing = {}
@@ -63,8 +70,14 @@ while True:
         ongoing = {item: queue[item]}
         del queue[item]
         
-    queue = dict(sorted(queue.items(), key=lambda x: x[1][2]))
-    
+    #if ongoing is empty and queue isn't, transfer first item in queue to ongoing
+    elif not ongoing and queue:
+        item = min(queue, key=lambda x: queue[x][2])
+        ongoing = {item: queue[item]}
+        del queue[item]
+
+    queue = dict(sorted(queue.items(), key=lambda x: (x[1][2], x[1][3], x[0])))
+
     #Ongoing decrements its burstime as it passes through time
     if ongoing:
         ongoing_job = list(ongoing.keys())[0]
@@ -76,8 +89,8 @@ while True:
             completed[ongoing_job] = copyJobs[ongoing_job] + [tat, wT]
             del remainingJobs[ongoing_job]
             del ongoing[ongoing_job]
-    
-    
+
+    prev_queue = copy.deepcopy(queue)
     sortedCompleted = dict(sorted(completed.items()))
     #Print Logs
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -85,18 +98,19 @@ while True:
         Current Time: {currTime}
         Proccesor: {ongoing}
         Queue: {queue}
+        tat: {tat}
         Completed: {sortedCompleted}
         """)
     #Tabulate
     print("{:<2} {:<2} {:<2} {:<2} {:<2} {:<2}".format("JOB", "AT", "BT", "P", "TAT", "WT"))
     for job, values in sortedCompleted.items():
-        print("{:<3} {:<2} {:<2} {:<2} {:<3} {:<2}".format(job, values[0], values[1], values[2], values[3], values[4]))
-    
-    
-    
+        print("{:<3} {:<2} {:<2} {:<2} {:<3} {:<2}".format(job, values[0], values[1], values[2], values[4], values[5]))
+
+
+
     #increment to increase time
     currTime += 1
-    
+
     #if no more jobs and met the expected eta
     if currTime == eta + 1:
         break
